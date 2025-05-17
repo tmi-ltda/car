8cb414a669cd1a3b67dac0d833724ff5e6e963b6
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-17 17:54:26 -0300

    refactor: changed the config param to a const

--- src/Car.hpp
+++ src/Car.hpp
@@ -11,7 +11,7 @@ class Car {
     uint16_t _speed;
   
   public:
-    Car(car_config_t& config)
+    Car(const car_config_t& config)
       : _leftMotor(config.left_pin1, config.left_pin2), _rightMotor(config.right_pin1, config.right_pin2), _speed(4095) {}
     
     void forward() {
