c69324d420e9efc9c855bfc9a5fcad7c21c5d2bf
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-19 15:34:35 -0300

    feat: defined max power function

--- src/Car.hpp
+++ src/Car.hpp
@@ -7,35 +7,40 @@
 
 class Car {
   private:
-    DCMotor _leftMotor, _rightMotor;
+    DCMotor _left_motor, _right_motor;
     uint16_t _speed;
   
   public:
     Car(const car_config_t& config)
-      : _leftMotor(config.left_pin1, config.left_pin2), _rightMotor(config.right_pin1, config.right_pin2), _speed(4095) {}
+      : _left_motor(config.left_pin1, config.left_pin2), _right_motor(config.right_pin1, config.right_pin2), _speed(4095) {}
     
     void forward() {
-      _leftMotor.forward();
-      _rightMotor.forward();
+      _left_motor.forward();
+      _right_motor.forward();
     }
 
     void backward() {
-      _leftMotor.backward();
-      _rightMotor.backward();
+      _left_motor.backward();
+      _right_motor.backward();
     }
 
     void left() {
-      _leftMotor.stop();
-      _rightMotor.forward();
+      _left_motor.stop();
+      _right_motor.forward();
     }
 
     void right() {
-      _leftMotor.forward();
-      _rightMotor.stop();
+      _left_motor.forward();
+      _right_motor.stop();
     }
 
     void stop() {
-      _leftMotor.stop();
-      _rightMotor.stop();
+      _left_motor.stop();
+      _right_motor.stop();
+    }
+
+    void setMaxPower(float power) {
+      _left_motor.setMaxPower(power);
+      _right_motor.setMaxPower(power);
     }
 };
