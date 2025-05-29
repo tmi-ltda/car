262ad7e8f6216720feb211aad2a7d79a74c70de8
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-29 12:41:04 -0300

    refactor: declaração da estrutura de configuração no arquivo principal

--- include/car_config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-typedef struct {
-  uint8_t left_pin1, left_pin2, right_pin1, right_pin2;
-} car_config_t;
--- src/Car.hpp
+++ src/Car.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
-#include <car_config.h>
 #include <DCMotor.hpp>
 #include <stdint.h>
 #include <Arduino.h>
+
+typedef struct {
+  uint8_t left_pin1, left_pin2, right_pin1, right_pin2;
+} car_config_t;
 
 class Car {
   private:
